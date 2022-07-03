22f0561daea56f81da7afdec270c5c646394051f
Yasantha Mihiran <yasanthamax@gmail.com>
2022-07-03 16:00:18 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8070;
+
+//Connect database
+mongoose.connect(process.env.DATABASE, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    .then(() => {
+      console.log("MongoDB Connected...");
+    })
+    .catch((err) => {
+      console.log("MongoDB connection Failed...", err.message);
+      process.exit();
+});
+
+
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => {
+  res.json("Server Started");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
